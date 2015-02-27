13371a17e816a3c00a3882841d9f6effe5ab1e49
Damian Gerow <damian.gerow@shopify.com>
2015-02-27 15:06:19 +0000

    More accurate function name; ugly support for proxy headers

--- rocket_launcher.rb
+++ rocket_launcher.rb
@@ -6,12 +6,18 @@ pid = nil
 get '/' do
   if params[:new] && pid
     Process.kill("SIGKILL", pid)
-    pid = spawn_process(params[:new] == 'ip' ? request.ip : params[:new])
+
+    # request.ip isn't honouring proxy headers, so do it manually
+    client_ip = @env['HTTP_X_FORWARDED_FOR'] || request.ip
+    host = params[:new] == 'ip' ? client_ip : params[:new]
+
+    pid = spawn_proxy(host)
   end
-  pid ||= spawn_process
-  "PID is #{pid}"
+  pid ||= spawn_proxy
+
+  "em-proxy launched as PID #{pid}, using host '#{host}' as upstream proxy"
 end
 
-def spawn_process(host="localhost")
+def spawn_proxy(host="127.0.0.1")
   Process.spawn("em-proxy -l 8080 -r #{host}:3000 -v")
 end
